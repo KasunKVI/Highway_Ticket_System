87c3765d841f2a7575ecac9f7d58a8c9d1ea1d00
KasunKVI <kkhettiarachchi10@gmail.com>
2024-07-02 14:31:20 +0530

    implement the dto for ticket status changing

---
+++ payment-service/payment-service/src/main/java/software/kasunkavinda/payment_service/dto/StatusDTO.java
@@ -0,0 +1,17 @@
+package software.kasunkavinda.payment_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class StatusDTO {
+
+    private String id;
+    private String status;
+
+}
